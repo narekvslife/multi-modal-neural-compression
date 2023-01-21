6cb8c97e3aad66393b0c48155803d9a713f71e34
Narek Alvandian <naalvandyan@gmail.com>
2023-01-21 18:56:09 +0100

    If this works I quit programming i promise

--- runai_launch.sh
+++ runai_launch.sh
@@ -2,7 +2,4 @@ wandb login ${WANDB_API_KEY}
 
 git pull
 
-
-python src/train.py --pretrained $PRETRAINED --model $MODEL_NUMBER --quality $QUALITY --tasks $TASKS --dataset $DATASET --lmbda $LAMBDA --epochs $EPOCHS --latent-size $LATENT_SIZE --learning-rate-main $LEARNING_RATE_MAIN --learning-rate-aux $LEARNING_RATE_AUX --num-workers $NUM_WORKERS --batch-size $BATCH_SIZE --devices $DEVICES --accelerator $ACCELERATOR
-
-python src/train.py --pretrained $PRETRAINED --model $MODEL_NUMBER --quality $QUALITY --tasks $TASKS --dataset $DATASET --lmbda $LAMBDA --epochs $EPOCHS --latent-size $LATENT_SIZE --learning-rate-main $LEARNING_RATE_MAIN --learning-rate-aux $LEARNING_RATE_AUX --num-workers $NUM_WORKERS --batch-size $BATCH_SIZE --devices $DEVICES --accelerator $ACCELERATOR
+python src/train.py --dataset ${DATASET} --tasks ${TASKS} --model ${MODEL_NUMBER} --pretrained ${PRETRAINED}  --quality ${QUALITY} --lmbda ${LAMBDA} --epochs ${EPOCHS} --latent-size ${LATENT_SIZE} --learning-rate-main ${LEARNING_RATE_MAIN} --learning-rate-aux ${LEARNING_RATE_AUX} --num-workers ${NUM_WORKERS} --batch-size ${BATCH_SIZE} --devices ${DEVICES} --accelerator ${ACCELERATOR}
